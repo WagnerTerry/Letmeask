98ca3ff8038e4275f6aa01cea4898afec3663e61
Wagner Terry <goncalveswagner15@gmail.com>
2022-02-15 23:35:19 -0300

    aula 05 - controle do admin e deploy firebase

--- README.md
+++ README.md
@@ -9,3 +9,16 @@
 * yarn add node-sass@^5.0.0
 * yarn add react-router-dom
 * yarn add @types/react-router-dom
+* yarn add classnames
+
+- deploy firebase
+
+* npm install -g firebase-tools ou yarn global add firebase-tools
+
+- firebase login
+- firebase init
+- marcar Realtime Database e Hosting: configure files... github action deploys
+- use an existing project
+- what do you ... public directory? build
+- yarn build
+- firebase deploy
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useCallback } from "react";
 import { useHistory } from "react-router-dom";
 
 import illustrationImg from "../assets/images/illustration.svg";
@@ -6,44 +7,47 @@ import googleIconImg from "../assets/images/google-icon.svg";
 
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
-
 import { useAuth } from "../hooks/useAuth";
-import { FormEvent, useState } from "react";
+import { useState } from "react";
 import { database } from "../services/firebase";
 
-export function Home() {
+export const Home = () => {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
   const [roomCode, setRoomCode] = useState("");
 
-  async function handleCreateRoom() {
+  const handleCreateRoom = useCallback(async () => {
     if (!user) {
       await signInWithGoogle();
     }
+
     history.push("/rooms/new");
-  }
+  }, [history, user, signInWithGoogle]);
 
-  async function handleJoinRoom(event: FormEvent) {
-    event.preventDefault();
+  const handleJoiRoom = useCallback(
+    async (event: FormEvent) => {
+      event.preventDefault();
 
-    if (roomCode.trim() === "") {
-      return;
-    }
+      if (roomCode.trim() === "") {
+        return;
+      }
 
-    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+      const roomRef = await database.ref(`rooms/${roomCode}`).get();
 
-    if (!roomRef.exists()) {
-      alert("Room does not exists");
-      return;
-    }
+      if (!roomRef.exists()) {
+        alert("Room is not exists.");
+        return;
+      }
 
-    if (!roomRef.val().endedAt) {
-      alert("Room already closed");
-      return;
-    }
+      if (roomRef.val().endedAt) {
+        alert("Room already closed.");
+        return;
+      }
 
-    history.push(`rooms/${roomCode}`);
-  }
+      history.push(`/rooms/${roomCode}`);
+    },
+    [history, roomCode]
+  );
 
   return (
     <div id="page-auth">
@@ -60,14 +64,14 @@ export function Home() {
           <img src={logoImg} alt="letmeask" />
           <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
-            Crie sua sala com o Google
+            Crie sua sala com o google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form onSubmit={handleJoinRoom}>
+          <form onSubmit={handleJoiRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"
-              onChange={(e) => setRoomCode(e.target.value)}
+              onChange={(event) => setRoomCode(event.target.value)}
               value={roomCode}
             />
             <Button type="submit">Entrar na sala</Button>
@@ -76,4 +80,4 @@ export function Home() {
       </main>
     </div>
   );
-}
+};
